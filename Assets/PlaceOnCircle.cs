4552b03923b1442452d1fa4c06662904e602db5b
Francois Dervaux <francois.dervaux@gmail.com>
2024-01-24 08:53:00 +0100

    update

--- Assets/PlaceOnCircle.cs
+++ Assets/PlaceOnCircle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using JfranMora.Inspector;
+using UnityEditor;
 using UnityEngine;
 
 public class PlaceOnCircle : MonoBehaviour
@@ -10,21 +11,25 @@ public class PlaceOnCircle : MonoBehaviour
     [SerializeField] private GameObject prefab;
 
     [Button]
-    public void UpdateObjects()
+    private void DestroyChilds()
     {
         //delete children
-        foreach (Transform child in transform)
+        for (int index = transform.childCount -1 ; index >=0 ; --index)
         {
-            DestroyImmediate(child.gameObject);
+            DestroyImmediate(transform.GetChild(index).gameObject);
         }
+    }
 
+    [Button]
+    public void UpdateObjects()
+    {
         for (int i = 0; i < numberOfObjects; i++)
         {
             float angle = i * Mathf.PI * 2 / numberOfObjects;
             Vector3 positionOnCircle = new Vector3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0f);
-            positionOnCircle = transform.TransformPoint(positionOnCircle);
 
-            Instantiate(prefab, positionOnCircle, Quaternion.identity, transform);
+            GameObject prefabInstance = PrefabUtility.InstantiatePrefab(prefab, transform) as GameObject;
+            prefabInstance.transform.localPosition = positionOnCircle;
         }
     }
 }
